d26c414b99f9db6672d04047ed752a9399d827d5
JISUYANG148D\jisuyang <jisuyang@egoist.im>
2020-09-16 14:27:49 +0900

    [add] Game Page
    게임페이지의 기본셋과 그와 연결되는 index.html을 수정했습니다.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+var arFileName = ["gameSadari","gameJebi","gameDice","gameRussian","gameBottle"];
+
 // 게임 리스트 생성
 var arTitle = ["사다리타기","제비뽑기","주사위대결","러시안룰렛","병돌리기"];
 var arCount = ["2인 - 8인","2인 - 8인","2인","2인, 3인, 6인","2인 - 8인"];
@@ -13,7 +15,7 @@ for (var i = 0; i< arTitle.length; i++){
         "                            <h3 class=\"mb-2\">"+arTitle[i]+"</h3>\n" +
         "                        </div>\n" +
         "                        <div class=\"col-4\">\n" +
-        "                            <a href=\"#\" class=\"btn btn-full btn-s font-600 font-12 rounded-m gradient-"+arColor[i]+"\">시작</a>\n" +
+        "                            <a href=\"#\" data-menu=\"menu-"+arFileName[i]+"\" class=\"btn btn-full btn-s font-600 font-12 rounded-m gradient-"+arColor[i]+"\">시작</a>\n" +
         "                        </div>\n" +
         "                        <div class=\"col-10\">\n" +
         "                            <span class=\"badge bg-"+arColor[i]+"-dark pm-1\">"+arCount[i]+"</span><span class=\"badge pm-2\">벌칙 : "+arKill[i]+" · "+arMission[i]+"</span>\n" +
@@ -48,4 +50,29 @@ for(var i = 0; i < arTitle2.length; i++) {
     $("#tab-2").append(castList);
 }
 
+// 인원수 선택 리스트 생성
+var arrCountNum = [[2,3,4,5,6,7,8],[2,3,4,5,6,7,8],[2],[2,3,6],[2,3,4,5,6,7,8]];
+
+for(var i=0; i<arFileName.length;i++){
+    var menuOption = "";
+    for(var j=0; j<arrCountNum[i].length;j++){
+        menuOption += "                <a href=\""+arFileName[i]+".html?cnt="+arrCountNum[i][j]+"\"><span>"+arrCountNum[i][j]+"명</span><i class=\"fa fa-angle-right\"></i></a>\n";
+    }
+    var countList =
+        "<div id=\"menu-"+arFileName[i]+"\"\n" +
+        "         class=\"menu menu-box-modal rounded-m\"\n" +
+        "         data-menu-height=\""+(100+(arrCountNum[i].length)*50)+"\"\n" +
+        "         data-menu-width=\"300\">\n" +
+        "        <div class=\"menu-title\">\n" +
+        "            <h1 class=\"font-24\">참가인원</h1>\n" +
+        "            <a href=\"#\" class=\"close-menu\"><i class=\"fa fa-times-circle\"></i></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"mr-3 ml-3 mt-3\">\n" +
+        "            <div class=\"list-group list-custom-small\">\n" + menuOption +
+        "            </div>\n" +
+        "            <div class=\"clear\"></div>\n" +
+        "        </div>\n" +
+        "    </div>";
+    $("#page").append(countList);
+}
 
