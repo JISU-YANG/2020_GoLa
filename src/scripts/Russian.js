3d1cc575b0debbd4c8d330989ef302608e03a96c
pcolo <akkang92@naver.com>
2020-09-28 03:41:18 +0900

    러시안룰렛게임 기능 완료, css 부분수정
    러시안룰렛은 기능완료했습니다.
    그외 제비뽑기 css버튼수정, 최종css는 추후 예정입니다

--- src/scripts/Russian.js
+++ src/scripts/Russian.js
@@ -1,24 +1,27 @@
 var rotation = 3660; //회전각도
-var bang = [1,0,0,0,0,0];
-var count = 0;
+var bang = [1,0,0,0,0,0]; //임의의 총알
+var count = 0; //발사횟수
 
 
-
-var j, x, y;
-for(var i=0; i<999; i++){
-    j = Math.floor(Math.random() * 6);
-    y = j-1;
-    x = bang[j];
-    if(y < 0){
-        y = 5;
+//총알 섞기
+function shuffle(){
+    var j, x, y;
+    for(var i=0; i<999; i++){
+        j = Math.floor(Math.random() * 6);
+        y = j-1;
+        x = bang[j];
+        if(y < 0){
+            y = 5;
+        }
+        bang[j] = bang[y];
+        bang[y] = x;
     }
-    bang[j] = bang[y];
-    bang[y] = x;
 }
 
-
-
-$("#Start").click(function(){
+//탄창 빈탄창으로 이미지 교체
+     //총알 각각 넣기
+//시작버튼
+function Start(){
      $('#bullet').animate(
          { deg: 3600 },
          { duration: 3000, step: function(now) {
@@ -32,34 +35,59 @@ $("#Start").click(function(){
           $(".bulletOne").css('display','block');
      },3000);
 
+     shuffle();
      $("#Start").text('발사');
-     $("#Start").attr('id','fire');
+     $("#Start").attr('onclick','Fire()');
+     $("#Start").attr('id','Fire');
 
-    $("#handle").click(function(){
-         $('#bullet').animate(
-             { deg: rotation },
-             { duration: 300, step: function(now) {
+}
+
+
+function Fire(){
+    $('#bullet').animate(
+        { deg: rotation },
+        { duration: 300, step: function(now) {
+                $(this).css({
+                transform: 'rotate(' + now + 'deg)' });
+            }
+        }
+    );
+    rotation = rotation + 60;
+    if(count < 2){
+        $(".bulletOne").eq(count).delay(350).fadeOut( 'slow' );
+    }
+    else if(count > 2){
+        $(".bulletOne").eq(count-1).delay(350).fadeOut( 'slow' );
+    }
+    if(bang[count] == 0){
+        $("#title").text("생존!");
+    }else{
+        $(".divider").css('background','red');
+        $("#title").text("사망!");
+        $("#Fire").attr('onclick','rePlay()');
+        $("#Fire").text('다시하기');
+        $("#Fire").attr('id','rePlay');
+    }
+    count++;
+}
+
+function rePlay(){
+    rotation = 3660;
+    count = 0;
+     $('#bullet').animate(
+             { deg: 0 },
+             { duration: 1, step: function(now) {
                  $(this).css({
                      transform: 'rotate(' + now + 'deg)' });
                  }
              }
          );
-         rotation = rotation + 60;
-         if(count < 2){
-            $(".bulletOne").eq(count).delay(350).fadeOut( 'slow' );
-         }
-         else if(count > 2){
-             $(".bulletOne").eq(count-1).delay(350).fadeOut( 'slow' );
-         }
-         if(bang[count] == 0){
-            $("#title").text("생존!");
-         }else{
-            $(".divider").css('background','red');
-            $("#title").text("사망!");
-            $("#handle").off('click');
-         }
-
-         count++;
-
-    });
-});
+//    $("#bullet").css('transform','rotate(3600deg)');
+    $(".divider").css('background','white');
+    $("#title").text("");
+    $("#bullet").attr('src','../images/Russian/Bullet.png');
+    $(".bulletOne").css('display','none');
+    $("#rePlay").attr('onclick','Start()');
+    $("#rePlay").text('시작');
+    $("#rePlay").attr('id','Start');
+}
